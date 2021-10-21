d69bf180485eb56489ce1fc5d607e97a4ed486dd
Luiz Rioja <lulyrioja@gmail.com>
2021-10-20 23:43:25 -0300

    :alien: Implementing and styling the SendMessageForm component

---
+++ mobile/src/components/SendMessageForm/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { TextInput, View } from "react-native";
+import { COLORS } from "../../theme";
+import { Button } from "../Button";
+
+import { styles } from "./styles";
+
+export function SendMessageForm() {
+  const [message, setMessage] = useState("");
+  const [sendingMessage, setSendingmessage] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        keyboardAppearance="dark"
+        placeholder="Qual sua expectativa para o evento?"
+        placeholderTextColor={COLORS.GRAY_PRIMARY}
+        multiline
+        maxLength={140}
+        onChangeText={setMessage}
+        value={message}
+        editable={!sendingMessage}
+      />
+
+      <Button
+        title="ENVIAR MENSAGEM"
+        backgroundColor={COLORS.PINK}
+        color={COLORS.WHITE}
+      />
+    </View>
+  );
+}
