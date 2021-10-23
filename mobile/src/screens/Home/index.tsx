111ed776a7452fdb0ca5b09217f1c693bda13c82
Luiz Rioja <lulyrioja@gmail.com>
2021-10-22 23:56:00 -0300

    :alien: Rendering home based on authentication

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -5,16 +5,19 @@ import { Header } from "../../components/Header";
 import { MessageList } from "../../components/MessageList";
 import { SendMessageForm } from "../../components/SendMessageForm";
 import { SignInBox } from "../../components/SignInBox";
+import { useAuth } from "../../hooks/auth";
 
 import { styles } from "./styles";
 
 export function Home() {
+  const { user } = useAuth();
+
   return (
     <View style={styles.container}>
       <Header />
       <MessageList />
 
-      <SendMessageForm />
+      {user ? <SendMessageForm /> : <SignInBox />}
     </View>
   );
 }
