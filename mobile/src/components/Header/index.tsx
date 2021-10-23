412e2277703656a4d57b62f2db4df28abecb4038
Luiz Rioja <lulyrioja@gmail.com>
2021-10-22 23:56:34 -0300

    :alien: Rendering header photo dynamically

--- mobile/src/components/Header/index.tsx
+++ mobile/src/components/Header/index.tsx
@@ -4,18 +4,21 @@ import { Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 import LogoSvg from "../../assets/logo.svg";
 import { UserPhoto } from "../UserPhoto";
+import { useAuth } from "../../hooks/auth";
 
 export function Header() {
+  const { user, signOut } = useAuth();
+
   return (
     <View style={styles.container}>
       <LogoSvg />
 
       <View style={styles.logoutButton}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={signOut}>
           <Text style={styles.logoutText}>Sair</Text>
         </TouchableOpacity>
 
-        <UserPhoto imageUri="https://github.com/mrrioja.png" />
+        <UserPhoto imageUri={user?.avatar_url} />
       </View>
     </View>
   );
