0f2b4a5a54a2aa7def2b203730751aeaf2028cf3
Luiz Rioja <lulyrioja@gmail.com>
2021-10-23 23:24:49 -0300

    :alien: Implementing message queue and fetching messages from the backend

--- mobile/src/components/MessageList/index.tsx
+++ mobile/src/components/MessageList/index.tsx
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView } from "react-native";
-import { Message } from "../Message";
+import { api } from "../../services/api";
+import { io } from "socket.io-client";
+
+import { Message, MessageProps } from "../Message";
 
 import { styles } from "./styles";
 
+let messagesQueue: MessageProps[] = [];
+
+const socket = io(String(api.defaults.baseURL));
+
+socket.on("new_message", (newMessage) => {
+  messagesQueue.push(newMessage);
+});
+
 export function MessageList() {
-  const message = {
-    id: "1",
-    text: "Teste",
-    user: {
-      name: "username",
-      avatar_url: "https://github.com/mrrioja.png",
-    },
-  };
+  const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
+
+  useEffect(() => {
+    async function fetchMessages() {
+      const messagesResponse = await api.get<MessageProps[]>("/messages/last3");
+
+      setCurrentMessages(messagesResponse.data);
+    }
+
+    fetchMessages();
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setCurrentMessages((prevState) => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1],
+        ]);
+        messagesQueue.shift();
+      }
+    }, 3000);
+
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <ScrollView
@@ -20,9 +49,9 @@ export function MessageList() {
       contentContainerStyle={styles.content}
       keyboardShouldPersistTaps={"never"}
     >
-      <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
+      {currentMessages.map((message) => (
+        <Message key={message.id} data={message} />
+      ))}
     </ScrollView>
   );
 }
