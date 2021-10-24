7be525d42b25c5fb0f040f2713b8eb83089bb696
Luiz Rioja <lulyrioja@gmail.com>
2021-10-23 23:26:02 -0300

    :alien: Placing the KeyBoardAvoidingView on the home screen

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { KeyboardAvoidingView, Platform, View } from "react-native";
 
 import { Header } from "../../components/Header";
 import { MessageList } from "../../components/MessageList";
@@ -13,11 +13,16 @@ export function Home() {
   const { user } = useAuth();
 
   return (
-    <View style={styles.container}>
-      <Header />
-      <MessageList />
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+    >
+      <View style={styles.container}>
+        <Header />
+        <MessageList />
 
-      {user ? <SendMessageForm /> : <SignInBox />}
-    </View>
+        {user ? <SendMessageForm /> : <SignInBox />}
+      </View>
+    </KeyboardAvoidingView>
   );
 }
